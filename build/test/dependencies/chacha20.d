f40d346d79129b4bbb64ffa332384c2d0c28c38c
lautiq <lautaroquarin@gmail.com>
2024-08-27 22:26:56 -0300

    Coverage results added

--- build/test/dependencies/chacha20.d
+++
@@ -1,1 +0,0 @@
-build/test/out/c/chacha20.o: src/chacha20.c src/chacha20.h
--- build/test/dependencies/test_chacha.d
+++
@@ -1,3 +0,0 @@
-build/test/out/c/test_chacha.o: test/test_chacha.c src/chacha20.h \
- /var/lib/gems/3.0.0/gems/ceedling-0.31.1/vendor/unity/src/unity.h \
- /var/lib/gems/3.0.0/gems/ceedling-0.31.1/vendor/unity/src/unity_internals.h
